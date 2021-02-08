bc5069437393676d69f5ee5500a2e77fcd1be1f3
Vinnys Lins <vinnysflins@gmail.com>
2021-02-08 12:18:48 -0300

    test: coverage path ignore patterns

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,11 @@ export default {
     'src/**/*.ts'
   ],
   coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    'src/index.ts',
+    'src/database',
+    'src/models'
+  ],
   coverageProvider: 'v8',
   preset: 'ts-jest',
   testEnvironment: 'node'

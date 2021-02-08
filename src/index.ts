921f26ba29ad5bb1e084009e3b3b5875bbb91a1c
Vinnys Lins <vinnysflins@gmail.com>
2021-02-08 02:03:14 -0300

    fix: express port

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import 'dotenv/config'
 
 import app from './app'
 
-app.listen(3000, () => console.log('Server running at http://localhost:3000'))
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`))

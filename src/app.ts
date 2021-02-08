6a154c753f07855cc131d85a77559abcf4fba258
Vinnys Lins <vinnysflins@gmail.com>
2021-02-08 12:38:16 -0300

    chore: add morgan for logging

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import 'express-async-errors'
 import yaml from 'yamljs'
 import swaggerUi from 'swagger-ui-express'
+import morgan from 'morgan'
 
 import EmployeeRoutes from './routes/Employee'
 import Exception from './middlewares/Exception'
@@ -11,6 +12,7 @@ const swaggerDocument = yaml.load(path.join(__dirname, '../swagger.yaml'))
 
 const app = express()
 
+app.use(morgan('tiny'))
 app.use(express.json())
 app.use('/api', EmployeeRoutes)
 app.use(Exception)
